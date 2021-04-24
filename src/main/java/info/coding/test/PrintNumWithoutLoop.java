b4eef91167fa4b37df76df21165141bf6c4320cf
samckp <sam.mca.liet@gmail.com>
2021-04-24 12:52:43 +0530

    updated

--- src/main/java/info/coding/test/PrintNumWithoutLoop.java
+++ src/main/java/info/coding/test/PrintNumWithoutLoop.java
@@ -1,2 +1,19 @@
-package info.coding.test;public class PrintNumWithoutLoop {
+package info.coding.test;
+
+public class PrintNumWithoutLoop {
+
+    public static void main(String[] args) {
+
+        printNum(90);
+    }
+
+    public static void printNum(int num) {
+
+        if (num < 0) {
+            return;
+        } else {
+            printNum(num - 1);
+            System.out.println(num);
+        }
+    }
 }
